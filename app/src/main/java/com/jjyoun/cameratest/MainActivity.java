357732e2eecb42b64fc49201a3565942b18e803a
jinju0809 <jjyoun48@gmail.com>
2021-07-06 10:00:59 +0900

    CameraX java version first commit

---
+++ app/src/main/java/com/jjyoun/cameratest/MainActivity.java
@@ -0,0 +1,109 @@
+package com.jjyoun.cameratest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
+import static androidx.core.content.PermissionChecker.PERMISSION_GRANTED;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+    private static final int PERMISSIONS_REQUEST = 1;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(!hasPermissions()){
+            requestPermission();
+        }else{
+            startCamera();
+        }
+
+    }
+
+    private void startCamera(){
+
+        //CameraProvider 요청
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        PreviewView previewView = findViewById(R.id.previewView);
+
+        //CameraProvider 사용 가능 여부 확인
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                Preview preview = new Preview.Builder().build();
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+
+
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+                Camera camera = cameraProvider.bindToLifecycle(
+                        ((LifecycleOwner)this), cameraSelector, preview
+                );
+                //bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private boolean hasPermissions(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
+    }
+
+    private void requestPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[] {PERMISSION_CAMERA}, PERMISSIONS_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == PERMISSIONS_REQUEST){
+            if(allPermissionsGranted(grantResults)){
+                startCamera();
+            }else{
+                requestPermission();
+            }
+        }
+    }
+
+    private static boolean allPermissionsGranted(int[] grantResults){
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
